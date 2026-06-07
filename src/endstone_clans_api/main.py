79c71f914bd8321edae5bdffb2dada8185b3e1c2
niko-at-chalupa <nikolekrescendo@protonmail.com>
2026-06-07 21:21:59 +0800

    chore: register commands

--- src/endstone_clans_api/main.py
+++ src/endstone_clans_api/main.py
@@ -3,5 +3,27 @@ from endstone.plugin import Plugin
 class ClansApiPlugin(Plugin):
     api_version = "0.11"
 
+    commands = {
+        "clan": {
+            "description": "Manage or create a clan",
+            "usages": [
+                "/clan create <name: str>",
+                "/clan modify name <name: str>",
+                "/clan modify displayname <displayname: str>",
+                "/clan invite <player: player>",
+                "/clan kick <player: player>",
+                "/clan disband",
+            ],
+            "permissions": ["clans-api.command"],
+        }
+    }
+
+    permissions = {
+        "clans-api.command": {
+            "description": "Base permission for all endstone clans API commands",
+            "default": "op",
+        }
+    }
+
     def on_enable(self):
         self.register_events(self)
