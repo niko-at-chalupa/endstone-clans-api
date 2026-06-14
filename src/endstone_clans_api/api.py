cbb4edc5d2aa1f1ae3b6998c6c7b5ab967aceafd
niko-at-chalupa <nikolekrescendo@protonmail.com>
2026-06-14 20:04:34 +0800

    feat(events): add events module

--- src/endstone_clans_api/api.py
+++ src/endstone_clans_api/api.py
@@ -1,7 +1,8 @@
 from .types import Clan
 from .database import Database
+from .events import ClanEventManager, ClanEvent
 from endstone.plugin import PluginManager
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
    from .main import ClansApiPlugin 
@@ -13,7 +14,16 @@ class ClansApi:
 
     def __init__(self, clans_api_plugin: 'ClansApiPlugin'):
         self.plugin = clans_api_plugin
+        self._event_manager = ClanEventManager(clans_api_plugin)
 
     @property
     def db(self) -> Database:
         return self.plugin.db
+
+    def register_events(self, listener: Any) -> None:
+        """Registers clan event handlers."""
+        self._event_manager.register_events(listener)
+
+    def call_event(self, event: ClanEvent) -> None:
+        """Calls clan event handlers."""
+        self._event_manager.call_event(event)
---
+++ src/endstone_clans_api/events.py
@@ -0,0 +1,89 @@
+from abc import ABC
+from typing import Callable, Type, Any, get_type_hints
+import inspect
+from endstone.event import EventPriority
+
+class ClanEvent(ABC):
+    @property
+    def event_name(self) -> str:
+        return self.__class__.__name__
+
+class ClanCancellableEvent(ClanEvent):
+    def __init__(self):
+        self._cancelled = False
+
+    @property
+    def cancelled(self) -> bool:
+        return self._cancelled
+
+    @cancelled.setter
+    def cancelled(self, value: bool):
+        self._cancelled = value
+
+    def cancel(self):
+        self._cancelled = True
+
+def clan_event_handler(func=None, *, priority: EventPriority = EventPriority.NORMAL, ignore_cancelled: bool = False):
+    """
+    Decorator to register an event handler.
+
+    The first argument of the decorated method must be a subclass of ClanEvent.
+
+    # Example
+    ```python
+    @clan_event_handler
+    def on_some_event(event: SomeClanEvent):
+        ...
+    ```
+    """
+    def decorator(f):
+        setattr(f, "_is_clan_event_handler", True)
+        setattr(f, "_clan_priority", priority)
+        setattr(f, "_clan_ignore_cancelled", ignore_cancelled)
+        return f
+
+    if func:
+        return decorator(func)
+
+    return decorator
+
+class ClanEventManager:
+    def __init__(self, plugin):
+        self._plugin = plugin
+        self._handlers: dict[Type[ClanEvent], list[Callable[[Any], Any]]] = {}
+
+    def register_events(self, listener: Any) -> None:
+        for attr_name in dir(listener):
+            attr = getattr(listener, attr_name)
+            if not callable(attr) or not getattr(attr, "_is_clan_event_handler", False):
+                continue
+
+            hints = get_type_hints(attr)
+            hints.pop("return", None)
+            
+            event_type = next(iter(hints.values()), None)
+
+            if not inspect.isclass(event_type) or not issubclass(event_type, ClanEvent):
+                self._plugin.logger.error(f"Failed to register clan event handler {attr_name}: No ClanEvent type hint found.")
+                continue
+
+            if event_type not in self._handlers:
+                self._handlers[event_type] = []
+            
+            self._handlers[event_type].append(attr)
+            self._handlers[event_type].sort(key=lambda x: getattr(x, "_clan_priority").value)
+
+    def call_event(self, event: ClanEvent) -> None:
+        for registered_type, handlers in self._handlers.items():
+            if isinstance(event, registered_type):
+                handler: Callable[[Any], Any]
+                for handler in handlers:
+                    if isinstance(event, ClanCancellableEvent) and event.cancelled and not getattr(handler, "_clan_ignore_cancelled"):
+                        continue
+                    try:
+                        handler(event)
+                    except Exception as e:
+                        handler_name = getattr(handler, "__name__", str(handler))
+                        self._plugin.logger.error(f"Error while calling clan event handler {handler_name}: {e}")
+                        import traceback
+                        self._plugin.logger.error(traceback.format_exc())
